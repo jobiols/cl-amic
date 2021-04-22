f2560cc3e9af24c947a85e50e9c8f1918d33bfdf
Jorge Obiols <jorge.obiols@gmail.com>
2021-04-22 08:50:53 -0300

    [FIX] agregar campos en QC issues

--- custom_qc/models/qc_issue_form.py
+++ custom_qc/models/qc_issue_form.py
@@ -4,6 +4,15 @@ from openerp import models, fields
 class QcIssueForm(models.Model):
     _inherit = "qc.issue"
 
-    workcenter_id = fields.Many2one(
-        'mrp.workcenter'
+    workcenter_1_id = fields.Many2one(
+        'mrp.workcenter',
+    )
+    workcenter_2_id = fields.Many2one(
+        'mrp.workcenter',
+    )
+    workcenter_3_id = fields.Many2one(
+        'mrp.workcenter',
+    )
+    issue_date = fields.Date(
+
     )
