20c0728c5b0769626b4f5cfbbe8df70067849eaa
Jorge Obiols <jorge.obiols@gmail.com>
2021-11-27 13:47:28 -0300

    [ADD] Reporte de etiquetas

---
+++ product_label_amic/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "product_label_amic",
+
+    'summary': """
+        Agrega reporte de etiquetas en el boton imprimir de una transferencia""",
+
+    'description': """
+        Reporte de etiquetas
+    """,
+
+    'author': "Joaquin Romero",
+    'website': "http://www.jjvrsistemas.com.ar",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Reportes',
+    'version': '11.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock','product'],
+
+    # always loaded
+    'data': [
+        'views/facturapreimpreso.xml',
+    ],
+}
